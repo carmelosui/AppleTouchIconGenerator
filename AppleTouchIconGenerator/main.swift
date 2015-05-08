12668f0556d5cba741b88cba99549aace43a3518
Carmelo Sui <Carmelosui@gmail.com>
2015-05-08 15:55:49 +0800

    Initial Commit

---
+++ AppleTouchIconGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppleTouchIconGenerator
+//
+//  Created by Carmelo Sui on 5/8/15.
+//  Copyright (c) 2015 Carmelo Sui. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
